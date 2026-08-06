ebc73d1c39bf0f7cd2605c441e3fb0c3eea6968d
phock12 <perrinhock12@gmail.com>
2026-08-06 07:47:56 -0500

    Design Draft Update
    Further class creation system for application cohesion, layout, and compatibility attempt. Dark Mode Toglee added. Page Wrapper added. Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Favorites from "./pages/Favorites";
-import Navbar from "./components/Navbar";
+
+import "./App.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    return localStorage.getItem("darkMode") === "true";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", darkMode);
+    document.body.classList.toggle("dark-mode", darkMode);
+  }, [darkMode]);
+
   return (
     <BrowserRouter>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Favorites" element={<Favorites />} />
-        </Routes>
+      <div className="app-container">
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+
+        <div className="page-wrapper">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
